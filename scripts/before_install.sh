8221db13818f0df45d1b90ccfe8b0fa3a7952ae4
unknown <rahmadfauzi600@gmail.com>
2024-09-18 19:21:57 +0700

    fix script

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,4 +2,4 @@
 # Stop any existing Node.js server
 pm2 stop nextjs-app || true
 # Clean the destination directory
-sudo rm -rf /home/ubuntu/next-todo-app/*
+# sudo rm -rf /home/ubuntu/next-todo-app/*
